f85430969282e6f787b9ba13351f758982d94563
sp724 <sp724@yahoo.com>
2018-07-20 22:25:42 -0400

    Updated

--- helloworld.py
+++ helloworld.py
@@ -234,5 +234,72 @@ from operator import attrgetter
 users_sorted = sorted(users,key=attrgetter('user_id'),reverse=True)
 print("Recipe 1.14:",users_sorted)
 
-# Recipe 1.15
+# Recipe 1.15 Grouping Records Together Based on a Field
 
+rows = [
+{'address': '5412 N CLARK', 'date': '07/01/2012'},
+{'address': '5148 N CLARK', 'date': '07/04/2012'},
+{'address': '5800 E 58TH', 'date': '07/02/2012'},
+{'address': '2122 N CLARK', 'date': '07/03/2012'},
+{'address': '5645 N RAVENSWOOD', 'date': '07/02/2012'},
+{'address': '1060 W ADDISON', 'date': '07/02/2012'},
+{'address': '4801 N BROADWAY', 'date': '07/01/2012'},
+{'address': '1039 W GRANVILLE', 'date': '07/04/2012'},
+]
+
+from operator import itemgetter
+from itertools import groupby
+
+# Sort by the desired field first
+
+rows.sort(key=itemgetter('date'))
+
+# Iterate in groups
+
+for date, items in groupby(rows, key=itemgetter('date')):
+    print("Recipe 1.15:",date)
+    for i in items:
+        print("Recipe 1.15:", i)
+
+# 1.16. Filtering Sequence Elements
+
+mylist = [1, 4, -5, 10, -7, 2, 3, -1]
+
+listComprehension = [n for n in mylist if n > 0]
+print("Recipe 1.16:",listComprehension)
+
+listComprehension = [n for n in mylist if n < 0]
+print("Recipe 1.16:",listComprehension)
+
+values = ['1', '2', '-3', '-', '4', 'N/A', '5']
+
+def is_int(val):
+    try:
+        x = int(val)
+        return True
+
+    except ValueError:
+        return False
+
+ivals = list(filter(is_int, values))
+print("Recipe 1.16:", ivals)
+
+# 1.17. Extracting a Subset of a Dictionary
+
+prices = {
+'ACME': 45.23,
+'AAPL': 612.78,
+'IBM': 205.55,
+'HPQ': 37.20,
+'FB': 10.75
+}
+
+# Make a dictionary of all prices over 200
+p1 = { key:value for key, value in prices.items() if value > 200 }
+
+# Make a dictionary of tech stocks
+tech_names = { 'AAPL', 'IBM', 'HPQ', 'MSFT' }
+
+p2 = { key:value for key, value in prices.items() if key in tech_names }
+
+print("Recipe 1.17:", p1, p2)
